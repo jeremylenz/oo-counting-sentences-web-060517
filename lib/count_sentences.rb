ac14538fb2f1ba6675896cbc3a5b2ea4ead11e0e
jeremylenz <jeremylenz@gmail.com>
2017-06-08 17:06:21 -0400

    flatten exclamation markkkk

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    split_self = self.split(".")
+    split_self.delete_if { |chunk| chunk == "" }
+    # binding.pry
+    # Now we have an array split by periods with extra ones removed
+    # binding.pry
+    ["?","!"].each do |punctuation_mark|
+      split_self.map! do |piece|
+        piece.split(punctuation_mark)
+      end
+      # Flatten as many times as we need to
 
+      while split_self[0].class == Array
+        split_self.flatten!
+      end
+      # Delete the blank strings from the array
+      split_self.delete_if { |chunk| chunk == "" }
+      # binding.pry
+    end
+# The length of the array should be the number of sentences 
+  split_self.length
   end
+
 end
